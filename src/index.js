4d3fddf7e42b36f2bcc041be0c2163b147ccfef0
madhav <you@example.com>
2021-02-21 00:53:38 +0530

    first version

---
+++ src/index.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var app = express();
+var hbs = require("hbs");
+var path = require("path");
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const { format } = require("path");
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./hbs"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/getInfo", async (req, res) => {
+  let url = req.query.url;
+  console.log("dwnload called", url);
+  const info = await ytdl.getBasicInfo(url).catch((err) => {
+    res.json({
+      error: err,
+    });
+    return;
+  });
+  
+  res.send({
+    info: info,
+  });
+});
+app.get("/download", async (req, res) => {
+  // res.header('Content-Disposition','attachment, filename="video.mp4"');
+  let url = req.query.url;
+  let fileName = req.query.fileNm;
+  url = JSON.parse(url);
+  console.log(url)
+  res.header("Content-Disposition", `attachment; filename=${fileName}.mp3`);
+console.log('donwload hit')
+  ytdl(url, {filter:'audioonly' })
+    .on("progress", (length, download, totallenght) => {
+    })
+    .pipe(res);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("port is running on", process.env.PORT);
+});
---
+++ src/public/js/main.js
@@ -0,0 +1,40 @@
+// const handleBars = require("hbs");
+// const template = handleBars.compile("Name:{{name}}");
+// console.log(template);
+let response;
+var fileName;
+let urlElement = document.getElementById("input-url");
+var img = document.createElement("img");
+document.addEventListener("input", (event) => {
+  console.log("called");
+  ListenPaste(event.target.value);
+});
+async function ListenPaste() {
+  const urlElement = document.getElementById("input-url");
+  const url = urlElement.value;
+  fetch(`/getInfo?url=${url}`).then((resp) => {
+    console.log(
+      resp.json().then((resp) => {
+        const respElement = document.getElementById("resp");
+
+        img.src = resp.info.videoDetails.thumbnails[3].url;
+        const duplicateImg = document.querySelector("img");
+        console.log(duplicateImg)
+        if(duplicateImg)
+        {
+          duplicateImg.remove()
+        }
+        document.querySelector(".thumbnail").appendChild(img);
+        respElement.innerText = resp.info.videoDetails.title;
+        fileName = resp.info.videoDetails.title;
+      })
+    );
+  });
+}
+
+function Submit() {
+  const urlElement = document.getElementById("input-url");
+  const url = JSON.stringify(urlElement.value);
+  console.log("submit clicked");
+  window.location.href = `/download?url=${url}&fileNm=${fileName}`;
+}
